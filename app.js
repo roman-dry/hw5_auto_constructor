fb773473e9dacf7ccb12c838d6c55d309ad9a31e
roman-dry <romabop39@gmail.com>
2022-12-07 05:49:31 -0500

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function Auto(name) {
+function Auto(name, model, yearOfIssue, vin) {
 	this.name = name;
-	this.model = '';
-	this.yearOfIssue = 0;
-	this.vin = '';
+	this.model = model;
+	this.yearOfIssue = yearOfIssue;
+	this.vin = vin;
 }
 
 Auto.prototype.log = function () {
@@ -10,48 +10,43 @@ Auto.prototype.log = function () {
 }
 
 Auto.prototype.checkVin = function() {
-	if(this.vin.length = 17) {      //Vin-code складається з 17-ти символів
-		return true;
-	} else {
-      return false;
-   }
+	return this.vin.length === 17
+}
+
+function Auto_Fuel(name, model, yearOfIssue, vin, engineVolume, fuelConsumption) {
+	this.name = name;
+	this.model = model;
+	this.yearOfIssue = yearOfIssue;
+   this.vin = vin;
+   this.engineVolume = engineVolume;
+	this.fuelConsumption = fuelConsumption;	
 }
 
 Auto_Fuel.prototype = Object.create(Auto.prototype);
-
-function Auto_Fuel() {
-	this.engineVolume = '';
-	this.fuelConsumption = 0;
-}
 
 Auto_Fuel.prototype.showFuelConsumption = function() {
 	return this.engineVolume + 'L ' + this.fuelConsumption + 'L/100km';
 }
 
-Auto_Electric.prototype = Object.create(Auto.prototype);
-
-function Auto_Electric() {
-	this.batteryCapacity = '';
+function Auto_Electric(name, model, yearOfIssue, vin, batteryCapacity) {
+	this.name = name;
+	this.model = model;
+	this.yearOfIssue = yearOfIssue;
+   this.vin = vin;
+	this.batteryCapacity = batteryCapacity;
 }
+
+Auto_Electric.prototype = Object.create(Auto.prototype);
 
 Auto_Electric.prototype.showBatteryConfig = function() {
 	return this.batteryCapacity + ' kWh';
 }
 
-let toyota = new Auto_Electric();
-toyota.name = 'Toyota'
-toyota.model = 'Prius';
-toyota.yearOfIssue = 2022;
-toyota.vin = 'JH2PC35051M200020';
-toyota.batteryCapacity = '150';
+let volkswagen = new Auto_Fuel('Volkswagen', 'Touareg', 2014, 'IP7OL76432X200020', 3, 12);
+let toyota = new Auto_Electric('Toyota', 'Prius', 2022, 'JH2PC35051M200020', '150');
 
-let volkswagen = new Auto_Fuel();
-volkswagen.name = 'Volkswagen'
-volkswagen.model = 'Touareg';
-volkswagen.yearOfIssue = 2014;
-volkswagen.vin = 'IP7OL76432X200020';
-volkswagen.engineVolume = '3';
-volkswagen.fuelConsumption = 12;
+
+
 
 
 
